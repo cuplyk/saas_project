4ec9527cec6f34d21c2db6b1d3b739bed7abbbdf
cuplyk <121808859+cuplyk@users.noreply.github.com>
2025-05-02 22:12:29 +0000

    add PageVisit

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,13 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from visits.models import PageVisit
+import pathlib
+
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
 def home_page_views(request, *args, **kwargs):
     """
     This function handles the home page view.
     It returns a simple HTML response with a welcome message.
     """
-    my_context = {
-        "page_title": "Hello World",
-    }
-
+    
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = (page_qs.count() * 100.0) / qs.count()
+    except:
+        percent = 0
+    my_title = "My Page"
     html_template = "home.html"
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "percent": percent,
+        "total_visit_count": qs.count(),
+    }
+    PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
