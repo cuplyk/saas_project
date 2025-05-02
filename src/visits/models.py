f12b11f9c7e4d6c8fc60100b57b4e3bcbb219c88
cuplyk <121808859+cuplyk@users.noreply.github.com>
2025-05-02 22:28:23 +0000

    fix: NOT NULL constraint failed: new__visits_pagevisit.page

--- src/visits/models.py
+++ src/visits/models.py
@@ -4,6 +4,5 @@ from django.db import models
 class PageVisit(models.Model):
     # db -> table
     # id -> hidden -> primary key -> autofield -> 1, 2, 3, 4, 5
- 
     path = models.TextField(blank=True, null=True) # col
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True) # col
