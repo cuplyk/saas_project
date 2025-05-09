0bb61b7631e88f1d0d9d4b712c238f20982019d0
cuplyk <121808859+cuplyk@users.noreply.github.com>
2025-05-09 14:49:20 +0000

    set up .env and connect neon database

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#bft(rg=cg2-bmg4^tbnu!=vhbp8cqft4_lpb5q0tdt)05u@y^"
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
+DEBUG = config("DJANGO_DEBUG")
+
 
 ALLOWED_HOSTS = [
     ".railway.app", # Add your Railway app domain here
