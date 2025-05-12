02d20f605b956dfb2665c2fb4ea8e7aed8baf873
cuplyk <121808859+cuplyk@users.noreply.github.com>
2025-05-12 22:32:01 +0000

    fix ImportError: cannot import name 'config' from 'decouple'

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
-from decouple import config
+import locale
+try:
+    from decouple import config
+except ImportError:
+    raise ImportError("The 'python-decouple' package is not installed. Install it using 'pip install python-decouple'.")
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
