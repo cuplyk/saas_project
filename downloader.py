2494f7c810584ced65838fd1bfdfd21be6ca9438
cuplyk <121808859+cuplyk@users.noreply.github.com>
2025-05-12 21:01:29 +0000

    config static files

---
+++ downloader.py
@@ -0,0 +1,23 @@
+import requests
+from pathlib import Path
+
+def download_to_local(url:str, out_path:Path, parent_mkdir:bool=True):
+    """
+    Downloads a file from a given URL and saves it to the specified local path.
+    
+    Args:
+        url (str): The URL of the file to download.
+        out_path (Path): The local path where the file will be saved.
+    """
+    if not isinstance(out_path, Path):
+        raise ValueError(f"{out_path} must be a pathlib.Path object")
+    if parent_mkdir:
+        out_path.parent.mkdir(parents=True, exist_ok=True)
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for bad responses
+        # write the file out in binary mode to prevent any newline conversions
+        out_path.write_bytes(response.content)
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading {url}: {e}")
+        return False
