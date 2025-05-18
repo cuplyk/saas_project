0327a8db88d3c2a91d669e44614be7eb4270e110
cuplyk <121808859+cuplyk@users.noreply.github.com>
2025-05-18 20:53:55 +0000

    add: Register User

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login
 # Create your views here.
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
 
 def login_view(request, *args, **kwargs):
     """
     This function handles the login page view.
 
     """
+    my_title = "Login Page"
+    my_context = {
+        "page_title": my_title,
+    }
 
     if request.method == "POST":
         username = request.POST.get("username") or None 
@@ -18,6 +27,34 @@ def login_view(request, *args, **kwargs):
                 login(request, user)
                 print("log in here")
                 return redirect("/")
-    return render(request, "auth/login.html", {})
+    return render(request, "auth/login.html", my_context)
 
 
+def register_view(request, *args, **kwargs):
+    """
+    This function handles the register page view.
+
+    """
+    my_title = "Register Page"
+    my_context = {
+        "page_title": my_title,
+    }
+    if request.method == "POST":
+        #print(request.POST)
+        username = request.POST.get("username") or None 
+        email = request.POST.get("email") or None
+        password = request.POST.get("password") or None
+
+        #username_exist = User.objects.filter(username__iexact=username).exists()
+        #email_exist = User.objects.filter(email__iexact=email).exists()
+        try:
+            User.objects.create_user(
+                username=username,
+                email=email,
+                password=password
+            )
+        except:
+            pass
+
+
+    return render(request, "auth/register.html", my_context)
--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -2,12 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from .views import home_views, about_views
-
-from accounts.views import login_view
+from accounts.views import login_view, register_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("login/", login_view),
+    path("register/", register_view),
     path("about/", about_views),
     path("", home_views)
 ]
