99e88190021b63e1b8b3812cbc40961e6a8e878c
cuplyk <121808859+cuplyk@users.noreply.github.com>
2025-05-18 12:51:52 +0000

    HTML login form

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -7,14 +7,17 @@ def login_view(request, *args, **kwargs):
     This function handles the login page view.
 
     """
-    username = "root2"#request.POST.get("username")
-    password = "djangoadmin"#request.POST.get("password")
-    # Check if the user has the correct credentials
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        print("log in here")
-        return redirect("/")
+
+    if request.method == "POST":
+        username = request.POST.get("username") or None 
+        password = request.POST.get("password") or None
+        # Check if the user has the correct credentials
+        if all([username, password]):
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                print("log in here")
+                return redirect("/")
     return render(request, "auth/login.html", {})
 
 
