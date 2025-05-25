ec23b9944e98f377b3fb747f988a9f1900e67ae8
cuplyk <121808859+cuplyk@users.noreply.github.com>
2025-05-25 18:04:30 +0000

    add password protected page

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from django.urls import path, include
-from .views import home_views, about_views
+from .views import home_views, about_views, pw_protected_view
 
 from accounts.views import login_view, register_view
 
@@ -11,6 +11,6 @@ urlpatterns = [
     path("register/", register_view),
     path("about/", about_views),
     path("", home_views, name="home"),
-
+    path("protected/", pw_protected_view),
     path('accounts/', include('allauth.urls')),
 ]
