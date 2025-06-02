5a5c0587efe3faf9a1e62b559d89ad4746df865f
cuplyk <121808859+cuplyk@users.noreply.github.com>
2025-06-02 18:54:06 +0000

    user permissions

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 
 from accounts.views import login_view, register_view
 
 urlpatterns = [
-    path("", home_views, name="home"),
+    path("<username>/", views.profile_view, name="profile"),
 
 ]
